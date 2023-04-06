de81a6c896846a99a8ece4b131d6b0b818ff5114
Justin Ang <jrang@sfu.ca>
2023-04-06 13:20:18 -0700

    init typescript and linters

---
+++ src/index.ts
@@ -0,0 +1,24 @@
+import dotenv from "dotenv";
+import express, { type Request, type Response } from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+
+dotenv.config();
+const app = express();
+
+// Basic Configuration
+const port: number = process.env.PORT ? parseInt(process.env.PORT, 10) : 3000;
+
+
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static('public'));
+
+app.get("/", (req: Request, res: Response) => {
+  res.sendFile(`${process.cwd()}/views/index.html`);
+});
+
+app.listen(port, () => {
+  console.log(`Listening on http://localhost:${port}`);
+});
+
