ba3b72ddff49a27731fcaf6b57b5147d39759b18
Justin Ang <jrang@sfu.ca>
2023-05-06 01:37:30 -0700

    remove body-parser

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,17 @@
 import dotenv from 'dotenv';
 import express, { type Request, type Response } from 'express';
-import bodyParser from 'body-parser';
 import cors from 'cors';
-import { PrismaClient } from '@prisma/client';
 import exerciseRoutes from './routes/exerciseRoutes';
 
 dotenv.config();
 const app = express();
-const prisma = new PrismaClient();
 
 // Basic Configuration
 const port: number =
   process.env.PORT != null ? parseInt(process.env.PORT, 10) : 3000;
 
 app.use(cors());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.static('public'));
 
 app.get('/', (req: Request, res: Response) => {
