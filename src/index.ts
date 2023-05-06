e3f48ea85d8cc271115fbba7833c50052b698d14
Justin Ang <jrang@sfu.ca>
2023-05-06 01:37:00 -0700

    used express router

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import express, { type Request, type Response } from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 import { PrismaClient } from '@prisma/client';
+import exerciseRoutes from './routes/exerciseRoutes';
 
 dotenv.config();
 const app = express();
@@ -20,99 +21,6 @@ app.get('/', (req: Request, res: Response) => {
   res.sendFile(`${process.cwd()}/views/index.html`);
 });
 
-app.post('/api/users', async (req: Request, res: Response) => {
-  const { username } = req.body;
-
-  const user = await prisma.user.upsert({
-    where: { username: username },
-    create: { username: username },
-    update: {}, // if user exists, do not update
-  });
-  res.json({ _id: user.id.toString(), username: user.username });
-});
-
-app.get('/api/users', async (req: Request, res: Response) => {
-  const users = await prisma.user.findMany();
-
-  const response = users.map((user) => {
-    return {
-      _id: user.id.toString(),
-      username: user.username,
-    };
-  });
-
-  res.json(response);
-});
-
-app.post('/api/users/:id/exercises', async (req: Request, res: Response) => {
-  const { description, duration, date } = req.body;
-  const exerciseSession = await prisma.exerciseSession.create({
-    data: {
-      desc: description,
-      duration: parseInt(duration),
-      date: date ? new Date(date) : new Date(),
-      User: {
-        connect: {
-          id: parseInt(req.params.id),
-        },
-      },
-    },
-  });
-
-  const user = await prisma.user.findUnique({
-    where: { id: parseInt(req.params.id) },
-  });
-
-  res.json({
-    username: user?.username,
-    _id: exerciseSession.userId.toString(),
-    description: exerciseSession.desc,
-    duration: exerciseSession.duration,
-    date: exerciseSession.date.toDateString(),
-  });
-});
-
-app.get('/api/users/:id/logs', async (req: Request, res: Response) => {
-  const from = req.query.from ? new Date(req.query.from as string) : undefined;
-  const to = req.query.to ? new Date(req.query.to as string) : undefined;
-  const limit = req.query.limit
-    ? parseInt(req.query.limit as string)
-    : undefined;
-
-  const logs = await prisma.user.findUnique({
-    where: { id: parseInt(req.params.id) },
-    include: {
-      sessions: {
-        where: {
-          date: {
-            gte: from,
-            lt: to,
-          },
-        },
-      },
-    },
-  });
-
-  const filteredLogs = logs?.sessions.map((session) => {
-    return {
-      description: session.desc,
-      duration: session.duration,
-      date: session.date.toDateString(),
-    }
-  }).slice(0, limit);
-
-
-  const response = {
-    username: logs?.username,
-    _id: logs?.id.toString(),
-    count: filteredLogs?.length,
-    log: filteredLogs,
-  };
-
-  console.log(response);
-  console.log(filteredLogs);
-
-  res.json(response);
-});
+app.use('/api/users', exerciseRoutes);
 
 app.listen(port);
